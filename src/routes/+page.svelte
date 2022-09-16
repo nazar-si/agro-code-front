4142dc202c078393ee4645fdb09a651e2c635d21
nazar-si <nazari61@yandex.ru>
2022-09-16 23:27:22 +0300

    Added grid example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   let dark = false;
   import { Moon, Sun } from "tabler-icons-svelte";
+  let width = 20;
+  let height = 20;
 </script>
 
 <main>
@@ -16,10 +18,17 @@
   <button class="secondary">Secondary</button>
   <div class="label">Primary color</div>
   <div class="label secondary">Secondary color</div>
-  <div class="grid">
-    {#each Array.from(Array(10).keys()) as i}
-      {#each Array.from(Array(10).keys()) as i}
-        <div class="entry" />
+  <div class="grid" style:grid-template-columns="repeat({width}, 1fr)">
+    {#each Array.from(Array(height).keys()) as i}
+      {#each Array.from(Array(width).keys()) as j}
+        <div
+          class="entry"
+          style:opacity={Math.abs(
+            (Math.sin(j / 7 + 2) * Math.sin(i / 8 + 3)) / 2 +
+              Math.sin(i / 3) * Math.cos(j / 3)
+          ) +
+            Math.random() / 7}
+        />
       {/each}
     {/each}
   </div>
@@ -43,7 +52,21 @@
       @apply bg-secondary border-secondary-600 gdark:border-secondary-400;
     }
   }
-  .entry {
-    @apply w-4 h-4 bg-primary-500 rounded-sm;
+  .grid {
+    display: grid;
+
+    width: fit-content;
+    gap: 0.25rem;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    @apply border-1 border-gray-300 gdark:border-gray-600 rounded-md;
+    .entry {
+      box-shadow: 0 0 0 #0000;
+      @apply w-4 h-4 bg-primary gdark:bg-secondary-500 rounded-sm transition-all;
+      @apply hover:!opacity-100 hover:bg-blue-500 gdark:hover:bg-blue-500;
+      &:hover {
+        box-shadow: 0 0 15px 3px #0084ff;
+      }
+    }
   }
 </style>
