41840d960a18c6cb4fa4136dc638816beb6fa3ca
nazar-si <nazari61@yandex.ru>
2022-09-16 22:55:54 +0300

    removed cypress completely

--- cypress/e2e/test.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Drag test', () => {
-    it('Drag panel', () => {
-        cy.visit('http://127.0.0.1:5173/')
-  
-        cy.get("header").first().trigger("dragstart")
-        cy.get(".side-snap").trigger("drop")
-    })
-})
