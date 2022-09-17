bf24244ec58ae1994c47d678d917ddec653b2759
Al1en <39908226+Al-1-en@users.noreply.github.com>
2022-09-17 18:22:50 +0300

    add client.ts and testAPI

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
-
-const host = '/'
+export const ssr = false;
+const host = 'http://79.120.76.23:6969'
 
 export const loginAPI = (login: string, password: string) => {
     fetch(host + '/user/login', {
@@ -12,9 +12,9 @@ export const loginAPI = (login: string, password: string) => {
             password: password
         })
     })
-    .then((response) => response.json)
+    .then((response) => response.json())
     .then((json: any) => {
-        console.log(json.token)
+        console.log(json)
         localStorage.setItem('token', json.token)
     })
 }
@@ -30,43 +30,49 @@ export const registerAPI = (login: string, password: string) => {
             password: password
         })
     })
-    .then((response) => response.json)
+    .then((response) => response.json())
     .then((json: any) => {
         console.log(json.token)
         localStorage.setItem('token', json.token)
     })
 }
 
-export const getUserVinoyardsAPI = (token: string) => {
-    fetch(host + '/user/getVineyards', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({
-            token: token
+export const getUserVinoyardsAPI = () => {
+    let token = localStorage.getItem('token')
+        if(token){
+        fetch(host + '/user/getVineyards', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                token: token
+            })
         })
-    })
-    .then((response) => response.json)
-    .then((json: any) => {
-        console.log(json.token)
-    })
+        .then((response) => response.json())
+        .then((json: any) => {
+            console.log(json)
+        })
+    }
 }
 
-export const setUserVinoyardsAPI = (token: string, vinoyards: []) => {
-    fetch(host + '/user/setVineyards', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({
-            token: token,
-            vinoyards: vinoyards
+export const setUserVinoyardsAPI = (vinoyards: []) => {
+    let token = localStorage.getItem('token')
+    if(token){
+        fetch(host + '/user/setVineyards', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                token: token,
+                vinoyards: vinoyards
+            })
         })
-    })
-    .then((response) => response.json)
-    .then((json: any) => {
-        console.log(json.token)
-    })
+        .then((response) => response.json())
+        .then((json: any) => {
+            console.log(json)
+        })
+    }
 }
 
